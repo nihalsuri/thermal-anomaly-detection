66f57c259e67f553cb4ac0d3a4292b7e38f8fbf9
Nihal Suri <nihalsuri1619@gmail.com>
2022-09-18 18:15:12 +0200

    convert images to numpy arrays

---
+++ eda/images_as_matrix.py
@@ -0,0 +1,23 @@
+from keras_preprocessing import image 
+import numpy as np
+
+# making n X m matrix
+def img2np(path, list_of_filename, size = (64, 64)):
+    # iterating through each file
+    for fn in list_of_filename:
+        fp = path + fn
+        current_image = image.load_img(fp, target_size = size, 
+                                       color_mode = 'grayscale')
+        # covert image to a matrix
+        img_ts = image.img_to_array(current_image)
+        # turn that into a vector / 1D array
+        img_ts = [img_ts.ravel()]
+        try:
+            # concatenate different images
+            full_mat = np.concatenate((full_mat, img_ts))
+        except UnboundLocalError: 
+            # if not assigned yet, assign one
+            full_mat = img_ts
+    return full_mat
+
+
