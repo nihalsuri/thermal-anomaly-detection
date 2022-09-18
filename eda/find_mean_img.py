e4382f4d0cb5c5acd53fa6dc64d0097e1a7da017
Nihal Suri <nihalsuri1619@gmail.com>
2022-09-18 19:13:42 +0200

    updated mean images

--- eda/find_mean_img.py
+++ eda/find_mean_img.py
@@ -6,9 +6,5 @@ def fmi(full_mat, title, size = (64, 64)):
     mean_img = np.mean(full_mat, axis = 0)
     # reshape it back to a matrix
     mean_img = mean_img.reshape(size)
-    # plt.imshow(mean_img, vmin=0, vmax=255, cmap='Greys_r')
-    # plt.title(f'Mean {title}')
-    # plt.axis('off')
-    # plt.show()
     return mean_img
 
