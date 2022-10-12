0ef9398868758b686b259fc2eae5dd406a57b7b6
Nihal Suri <nihal@example.com>
2022-10-12 19:27:09 +0200

    updated illustration

--- cnn/transformsIllustration.py
+++ cnn/transformsIllustration.py
@@ -8,7 +8,7 @@ import torchvision.transforms as T
 
 
 plt.rcParams["savefig.bbox"] = 'tight'
-orig_img = Image.open(Path('assets') / '0.png')
+orig_img = Image.open(Path('assets') / '2.png')
 # if you change the seed, make sure that the randomly-applied transforms
 # properly show that the image can be both transformed and *not* transformed!
 torch.manual_seed(0)
@@ -37,3 +37,39 @@ def plot(imgs, with_orig=True, row_title=None, **imshow_kwargs):
             axs[row_idx, 0].set(ylabel=row_title[row_idx])
 
     plt.tight_layout()
+    plt.show()
+    
+
+
+# For mages : 
+resized_imgs = [T.Resize(size=size)(orig_img) for size in (30, 50, 100, orig_img.size)]
+plot(resized_imgs)
+center_crops = [T.CenterCrop(size=size)(orig_img) for size in (30, 50, 100, orig_img.size)]
+plot(center_crops)
+(top_left, top_right, bottom_left, bottom_right, center) = T.FiveCrop(size=(100, 100))(orig_img)
+plot([top_left, top_right, bottom_left, bottom_right, center])
+jitter = T.ColorJitter(brightness=.5, hue=.3)
+jitted_imgs = [jitter(orig_img) for _ in range(4)]
+plot(jitted_imgs)
+blurrer = T.GaussianBlur(kernel_size=(5, 9), sigma=(0.1, 5))
+blurred_imgs = [blurrer(orig_img) for _ in range(4)]
+plot(blurred_imgs)
+perspective_transformer = T.RandomPerspective(distortion_scale=0.6, p=1.0)
+perspective_imgs = [perspective_transformer(orig_img) for _ in range(4)]
+plot(perspective_imgs)
+rotater = T.RandomRotation(degrees=(0, 180))
+rotated_imgs = [rotater(orig_img) for _ in range(4)]
+plot(rotated_imgs)
+affine_transfomer = T.RandomAffine(degrees=(30, 70), translate=(0.1, 0.3), scale=(0.5, 0.75))
+affine_imgs = [affine_transfomer(orig_img) for _ in range(4)]
+plot(affine_imgs)
+resize_cropper = T.RandomResizedCrop(size=(32, 32))
+resized_crops = [resize_cropper(orig_img) for _ in range(4)]
+plot(resized_crops)
+
+
+# standard_imgs = [resized_imgs, center_crops, jitted_imgs, blurred_imgs, perspective_imgs, rotated_imgs, affine_imgs, resized_crops ]
+# fig, axs = plt.subplots(8, 5)
+
+# axs[0].plot(resized_imgs)
+# axs[0].plot(center_crops)
