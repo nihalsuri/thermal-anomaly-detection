7558e9d53c4f776f2af8ef348322db2de3d6bd38
Nihal Suri <nihal@example.com>
2022-10-24 16:08:11 +0200

    Updated nn

--- nns/two_layer_nn.py
+++ nns/two_layer_nn.py
@@ -4,9 +4,6 @@ import torch
 import torch.nn as nn
 import torch.optim as optim
 import torch.nn.functional as F
-from torch.utils.data import DataLoader
-import torchvision.datasets as datasets
-import torchvision.transforms as transforms 
 from customData import train_loader, test_loader, val_loader
 
 # Create Fully connected network 
